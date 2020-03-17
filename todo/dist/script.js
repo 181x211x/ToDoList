db05b2471697315d2164aafaba63b9ad9161e1ba
takatsuki daiki <181x211x@gmail.com>
2020-03-17 17:38:20 +0900

    遷移をさせれるようにした

--- todo/dist/script.js
+++ todo/dist/script.js
@@ -1,16 +1,142 @@
+const task_todo = new Vue({
+  el: '#todo_body',
+  data: {
+    items: [
+    ],
+    newItemTitle: '' //追加
+  },
+  methods: {  //methodsオプションをまるっと追加
+    addTodo: function(newTitle){
+      this.items.push({
+        title: newTitle,
+        isChecked: false
+      });
+      this.newItemTitle = '';
+      this.saveTodo();
+    },
+    deleteTodo: function(){
+      this.items = this.items.filter(function (item) {
+        return item.isChecked === false; //
+      });
+    this.saveTodo();
+    },
+    MovetoWorking: function(){
+      var move_item = [];
+      move_item = this.items.filter(function (item) {
+        return item.isChecked === true; //
+      });
+      for(let i=0;i<move_item.length;i++){
+      task_working.items.push({
+        title: move_item[i].title,
+        isChecked: false
+      }
+    );
+    }
+    this.saveTodo();
+  },
+    saveTodo: function(){
+      localStorage.setItem('items_todo', JSON.stringify(this.items));
+    },
+    loadTodo: function(){
+      this.items = JSON.parse( localStorage.getItem('items_todo') );
+      if( !this.items ){
+        this.items = [];
+      }
+    },
+  },
+  mounted: function(){
+    this.loadTodo();
+  },
 
-var item = '<div id="item"><input type="text" id="task_name" value="新規タスク"></input><button id="dlt">削除</button></div>'
+})
 
-$("#addToDo").on("click", function(){
-  $('#todo_body').append(item);
-});
+const task_working = new Vue({
+  el: '#working_body',
+  data: {
+    items: [
+    ],
+    newItemTitle: '' //追加
+  },
+  methods: {  //methodsオプションをまるっと追加
+    addTodo: function(newTitle){
+      this.items.push({
+        title: newTitle,
+        isChecked: false
+      });
+      this.newItemTitle = '';
+      this.saveTodo();
+    },
+    deleteTodo: function(){
+      this.items = this.items.filter(function (item) {
+        return item.isChecked === false; //
+      });
+      this.saveTodo();
+    },
+    MovetoDone: function(){
+      var move_item = [];
+      move_item = this.items.filter(function (item) {
+        return item.isChecked === true; //
+      });
+      for(let i=0;i<move_item.length;i++){
+      task_done.items.push({
+        title: move_item[i].title,
+        isChecked: false
+      }
+    );
+    }
+    this.saveTodo();
+  },
+    saveTodo: function(){
+      localStorage.setItem('items_working', JSON.stringify(this.items));
+      localStorage.setItem('items_done', JSON.stringify(task_done.items));
+    },
+    loadTodo: function(){
+      this.items = JSON.parse( localStorage.getItem('items_working') );
+      if( !this.items ){
+        this.items = [];
+      }
+    },
+  },
+  mounted: function(){
+    this.loadTodo();
+  },
 
+})
 
-$(function () {
-    $(document).on("click", "#dlt",function(e) {
-    $(this).siblings().remove();
-    $(this).unwrap();
-    $(this).remove();
+const task_done = new Vue({
+  el: '#done_body',
+  data: {
+    items: [
+    ],
+    newItemTitle: '' //追加
+  },
+  methods: {  //methodsオプションをまるっと追加
+    addTodo: function(newTitle){
+      this.items.push({
+        title: newTitle,
+        isChecked: false
+      });
+      this.newItemTitle = '';
+      this.saveTodo();
+    },
+    deleteTodo: function(){
+      this.items = this.items.filter(function (item) {
+        return item.isChecked === false; //
+      });
+    this.saveTodo();
+    },
+    saveTodo: function(){
+      localStorage.setItem('items_done', JSON.stringify(this.items));
+    },
+    loadTodo: function(){
+      this.items = JSON.parse( localStorage.getItem('items_done') );
+      if( !this.items ){
+        this.items = [];
+      }
+    },
+  },
+  mounted: function(){
+    this.loadTodo();
+  },
 
-  });
-});
+})
